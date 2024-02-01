a60a61ecf7fd3fa19dbb3ad595f5380c1ecb196d
lenovo <sendtoashish123@yahoo.com>
2024-02-01 18:36:30 +0530

    commit

--- Terraform/modules/vpcm/outputs.tf
+++ Terraform/modules/vpcm/outputs.tf
@@ -2,10 +2,10 @@ output "vpc_network" {
   value = google_compute_network.vpc_network.name
 }
 output "network-secondary-ip-ranges-id_pod"{
-  value = google_compute_subnetwork.subnet.secondary_ip_range[0].ip_cidr_range
+  value = google_compute_subnetwork.subnet.secondary_ip_range[0].range_name
 }
 output "network-secondary-ip-range-id_service"{
-  value = google_compute_subnetwork.subnet.secondary_ip_range[1].ip_cidr_range
+  value = google_compute_subnetwork.subnet.secondary_ip_range[1].range_name
 }
 output "subnetwork_name" {
   value = google_compute_subnetwork.subnet.name
